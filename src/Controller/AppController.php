19171429b2816670fc48da36e7dc0ea4a79e66b4
Minh Phuc <tmphucgl@gmail.com>
2018-07-16 23:42:41 +0700

    update code

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -92,8 +92,8 @@ class AppController extends Controller
         $this->set('isPage', 'homes');
         $this->set('keywords', 'thiet ke noi that can ho dep, thiết kế nội thất căn hộ đẹp, thiet ke noi that van phong hien dai, thiết kế nội thất văn phòng hiện đại, thiet ke noi that shop, thiết kế nội thất shop');
         $this->set('description', '+ P A H chuyên thiết kế và thi công Nội thất Văn phòng, Showroom, Shop, Biệt thự, Căn hộ theo phong cách Scandinavia &amp; Minimalism / Our Experts in Scandinavia &amp; Minimalism Interior Design');
-        $this->set('generator', 'CÔNG TY TNHH KIẾN TRÚC NỘI THẤT + P A H');
-        $this->set('title', 'Thiết kế kiến trúc, nội thất... - + P A H');
+        $this->set('generator', 'CÔNG TY TNHH KIẾN TRÚC NỘI THẤT MO');
+        $this->set('title', 'Thiết kế kiến trúc, nội thất MO');
 
         $menus = $this->Menus->find()
             ->contain('ParentMenus')
