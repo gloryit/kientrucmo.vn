80239bb36d0df676c7c1c2b1d84821892526437e
Minh Phuc <tmphucgl@gmail.com>
2018-07-16 22:40:04 +0700

    email

--- config/defines.development.php
+++ config/defines.development.php
@@ -38,6 +38,7 @@ define('EMAIL_DEFAULT', [
     'timeout' => 30,
     'username' => 'nguyenchau.hoanganh@gmail.com',
     'password' => 'wjxbkslyuyzqmsrn',
+    'from' => 'nguyenchau.hoanganh@gmail.com',
 ]);
 
 /**
